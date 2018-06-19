505cb3f214af59efd252a39ca98887dba8d03442
Adeline <adeline.contact@gmail.com>
2018-06-19 23:26:40 +0200

    level 13 à revoir

--- prog.js
+++ prog.js
@@ -5,6 +5,7 @@ var score = 0;
 var multiplier = 1;
 var price = 50;
 var autoclic = $('#autoclic');
+var bonus = $('#bonus');
 
 
 // Actions associées au bouton 'click' :
@@ -13,7 +14,6 @@ var autoclic = $('#autoclic');
 	{
 		score = score +1 * multiplier;
 		
-
 		if (score>=200)
 		{
 			setInterval(function()
@@ -21,14 +21,12 @@ var autoclic = $('#autoclic');
 				score = score +1 * multiplier;
 				display.html(score);
 			},1000);
-	
 		}
-		
+
 		display.html(score);
-
 	});
-
-
+		
+	
 // Actions associées au bouton 'Multiply' :
 
 	multiply.click(function augmenterMultiplicateur()
@@ -53,27 +51,52 @@ var autoclic = $('#autoclic');
 		
 	});
 
-
-
-var style1 = function style()
+// Actions associées au bouton 'Autoclicker' :
+	
+	autoclic.click(function()
+	{
+		if (score<500)
 		{
-			if (score< 50)
+			alert("Le prix de l'autoclicker est de 500, vous n'avez pas suffisamment de crédit !");
+		}
+		else
+		{
+			autoclic.prop('disabled' , true);
+		}
+
+	});
+
+	// Actions associées au bouton 'BONUS' :
+	
+		
+
+	bonus.click(function()
+	{
+		var timeLeft = 30;
+		var timerId = setInterval('countdown', 1000);
+
+			if (score<50)
 			{
-				autoclic.prop('disabled', true);
+				alert("Le prix du BONUS est de 5000, vous n'avez pas suffisamment de crédit !");
 			}
 			else
 			{
-				autoclic.prop('disabled', false);
+				score = score - 5000;
+do {
+
+    				timeLeft =	timeLeft--;
+}
+				while (timeLeft > 0)
+				{
+					if (timeLeft == 0) 
+			 		{
+    					clearTimeout(timerId);
+  					}
+  			 		else 
+  			 		{
+    					bonus.html(timeLeft);
+  					}}
+				bonus.prop('disabled' , true);
 			}
-		};
 
-
-
-
-
-// Actions associées au bouton 'Autoclicker' :
-	
-//	 autoclic.click(function()
-//	{
-		
- //});
+	});
