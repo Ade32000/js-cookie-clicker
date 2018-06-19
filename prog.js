dc29ba17e60fd38581af57a93f04ea87b1042f46
Adeline <adeline.contact@gmail.com>
2018-06-19 11:58:34 +0200

    level 1 to 8

--- prog.js
+++ prog.js
@@ -1,3 +1,29 @@
-var button = ;
-var bloc = ;
+var button = $('#click');
+var display = $('#display');
 var score = 0;
+var multiplier = 1;
+
+	$('#click').click(function()
+	{
+		score = score +1 * multiplier;	
+	
+			if(score < 0)
+			{
+				score = 0;
+			}
+
+			$('#display').html(score);
+	});
+
+
+	$('#multiply').click(function augmenterMultiplicateur()
+	{
+		multiplier++;
+		score = score - 50;
+		$('#multiply').html('Multiply' + '  x ' + multiplier);
+		
+	});
+
+
+
+	
