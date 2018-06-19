b5e74a91ee0af18403050dcefd99a7d7f56f6e88
Adeline <adeline.contact@gmail.com>
2018-06-19 16:56:23 +0200

    level 9 to 11

--- prog.js
+++ prog.js
@@ -1,29 +1,79 @@
 var button = $('#click');
 var display = $('#display');
+var multiply = $('#multiply');
 var score = 0;
 var multiplier = 1;
+var price = 50;
+var autoclic = $('#autoclic');
 
-	$('#click').click(function()
+
+// Actions associées au bouton 'click' :
+
+	button.click(function()
 	{
-		score = score +1 * multiplier;	
+		score = score +1 * multiplier;
+		
+
+		if (score>=200)
+		{
+			setInterval(function()
+			{
+				score = score +1 * multiplier;
+				display.html(score);
+			},1000);
 	
+		}
+		
+		display.html(score);
+
+	});
+
+
+// Actions associées au bouton 'Multiply' :
+
+	multiply.click(function augmenterMultiplicateur()
+	{
+		multiplier++;
+
+		score = score - price;
+
+		multiply.html('x ' + multiplier);
+
 			if(score < 0)
 			{
 				score = 0;
 			}
 
-			$('#display').html(score);
-	});
+		display.html(score);
 
+		price = price *2;
 
-	$('#multiply').click(function augmenterMultiplicateur()
-	{
-		multiplier++;
-		score = score - 50;
-		$('#multiply').html('Multiply' + '  x ' + multiplier);
+		multiply.html('x' + multiplier + ' Prix : ' + price);
+
 		
 	});
 
 
 
+var style1 = function style()
+		{
+			if (score< 50)
+			{
+				autoclic.prop('disabled', true);
+			}
+			else
+			{
+				autoclic.prop('disabled', false);
+			}
+		};
+
+
+
+
+
+// Actions associées au bouton 'Autoclicker' :
 	
+//	 autoclic.click(function()
+//	{
+		
+ //});
